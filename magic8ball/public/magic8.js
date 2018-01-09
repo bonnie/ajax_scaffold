30e8a875c6364bcd83eb50a7be97d8d4d330284e
Mateo Balcorta <misterrattail@gmail.com>
2018-01-09 11:12:44 -0800

    Add ajax call and render answer to page.

--- magic8ball/public/magic8.js
+++ magic8ball/public/magic8.js
@@ -1,1 +1,13 @@
 // write ajax code here
+const answer = document.getElementById('answer')
+
+document.getElementById('question-form')
+  .addEventListener('submit', (event) =>{
+    console.log(event.target.)
+    event.preventDefault()
+    fetch('/magic8response')
+      .then(response => response.text())
+      .then(prediction => 
+        answer.innerHTML = prediction
+      )
+  })
